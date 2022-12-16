ebc2683a26365974249820de44bcf89030712c78
meghaniankov <meghan.e.mccue@gmail.com>
2022-12-16 10:20:21 +0000

    refactor bird_watcher

--- bird-watcher/bird_watcher.go
+++ bird-watcher/bird_watcher.go
@@ -1,11 +1,9 @@
 package birdwatcher
 
-// TotalBirdCount return the total bird count by summing
-// the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
 	total := 0
-	for i := 0; i < (len(birdsPerDay)); i++ {
-		total = total + birdsPerDay[i]
+	for _, dayCount := range birdsPerDay {
+		total += dayCount
 	}
 	return total
 }
@@ -15,22 +13,15 @@ func TotalBirdCount(birdsPerDay []int) int {
 func BirdsInWeek(birdsPerDay []int, week int) int {
 	weekEnd := week * 7
 	weekStart := weekEnd - 7
-	total := 0
 
-	for i := weekStart; i < (weekEnd); i++ {
-		total = total + birdsPerDay[i]
-	}
-	return total
-
+	return TotalBirdCount(birdsPerDay[weekStart : weekEnd])
 }
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
 func FixBirdCountLog(birdsPerDay []int) []int {
-	for i := 0; i < (len(birdsPerDay)); i++ {
-		if (i %2 == 0) {
-			birdsPerDay[i]++
-		} 
+	for i := 0; i < (len(birdsPerDay)); i +=2 {
+		birdsPerDay[i]++
 	}
 	return birdsPerDay
 }
